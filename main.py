3f4aa0d3c14e5baf9fb3c8f39cf5558aebea5889
chstathis <stathischaritos@gmail.com>
2013-10-31 11:11:36 +0000

    main.py edited online with Bitbucket Added indexing where i save word counts per document and a global document frequency. We can use these values to make most of the IR models

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import os
 
 collection_directory = "/home/stathis/Projects/UVA_IR/collection/"
 
+index = {}
+doc_lengths = {}
+
 for root, dirs, files in os.walk(collection_directory):
     for file in files:
         if file.endswith(".txt"):
@@ -10,6 +13,21 @@ for root, dirs, files in os.walk(collection_directory):
             f = open(collection_directory + file,'r')
             raw_text = f.read()
             tokenized_text = nltk.word_tokenize(raw_text)
-            print tokenized_text
+            document = {}
+            doc_id = file[0:(len(file)-4)]
+            for term in tokenized_text:
+            	if term in document:
+            		document[term] += 1
+            	else:
+            		document[term] = 1
+            print document['the']
+            for word in document.iteritems():
+            	if word[0] not in index:
+            		index[word[0]] = {}
+            		index[word[0]]['df'] = 1
+            	else:
+            		index[word[0]]['df'] += 1
+            	index[word[0]][doc_id] = word[1]
             break
 
+print index['the']
